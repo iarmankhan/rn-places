77092045458b688cac6e35803dffb6bc2b1cb437
iarmankhan <armankhan9244@gmail.com>
2020-01-15 18:27:21 +0530

    Basic form setup, added redux & redux thunk

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
-import {Platform, Text, View} from "react-native";
+import {Button, Platform, StyleSheet, ScrollView, Text, TextInput, View} from "react-native";
+import Colors from "../constants/Colors";
 
 const NewPlaceScreen = props => {
+    const [title, setTitle] = useState('');
+
+    const titleChangeHandler = text => setTitle(text);
+
+    const savePlaceHandler = () => {
+
+    };
+
     return (
-        <View>
-            <Text>Add Place</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+            <View style={styles.formGroup}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={title} />
+            </View>
+            <Button title="Save Place" onPress={savePlaceHandler} color={Colors.primary} />
+            </View>
+        </ScrollView>
     )
 };
 
@@ -16,6 +31,26 @@ NewPlaceScreen.navigationOptions = navData => {
         headerTitle: "Add Place",
     }
 };
+
+const styles = StyleSheet.create({
+    form: {
+        margin:30,
+    },
+    formGroup: {
+
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
+});
 
 
 export default NewPlaceScreen
