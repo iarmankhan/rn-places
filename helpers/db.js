04d65e60823ab200ce9f13b8a5ace1766203e23e
iarmankhan <armankhan9244@gmail.com>
2020-01-16 12:25:07 +0530

    Storing data in db

--- helpers/db.js
+++ helpers/db.js
@@ -5,10 +5,28 @@ const db = SQLite.openDatabase('places.db');
 export const init = () => {
     return new Promise((resolve, reject) => {
         db.transaction(tx => {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, imageUri TEXT NOT NULL, address TEXT NOT NULL, lat REAL NOT NULL, lng REAL NOT NULL);',
+            tx.executeSql(
+                'CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, imageUri TEXT NOT NULL, address TEXT NOT NULL, lat REAL NOT NULL, lng REAL NOT NULL);',
                 [],
-                () => {
+                (_, result) => {
                     resolve()
+                },
+                (_, err) => {
+                    reject(err)
+                }
+            );
+        });
+    });
+};
+
+export const insertPlace = (title, imageUri, address, lat, lng) => {
+    return new Promise((resolve, reject) => {
+        db.transaction(tx => {
+            tx.executeSql(
+                'INSERT INTO places(title, imageUri, address, lat, lng) VALUES(?, ?, ?, ?, ?);',
+                [title, imageUri, address, lat, lng],
+                (_, result) => {
+                    resolve(result)
                 },
                 (_, err) => {
                     reject(err)
--- store/actions/places.js
+++ store/actions/places.js
@@ -1,5 +1,5 @@
 import * as FileSystem from "expo-file-system";
-
+import {insertPlace} from "../../helpers/db";
 
 export const ADD_PLACE = 'ADD_PLACE';
 
@@ -13,17 +13,21 @@ export const addPlace = (title, image) => {
                 from: image,
                 to: newPath
             });
+            const dbResult = await insertPlace(title, newPath, 'Dummy Address', 15.6, 12.3);
+
+            dispatch({
+                type: ADD_PLACE,
+                placeData: {
+                    id: dbResult.insertId,
+                    title: title,
+                    image: newPath,
+                }
+            });
         }catch (e) {
             console.log(e);
             throw e;
         }
 
-        dispatch({
-            type: ADD_PLACE,
-            placeData: {
-                title: title,
-                image: newPath,
-            }
-        });
+
     };
 };
