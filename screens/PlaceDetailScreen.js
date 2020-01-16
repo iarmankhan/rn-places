9adab5cf40a6d92b5db7885fa14384b3b3a516db
iarmankhan <armankhan9244@gmail.com>
2020-01-16 18:05:24 +0530

    Displaying the details screen

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -1,11 +1,25 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {ScrollView, StyleSheet, Image, Text, View} from "react-native";
+
+import MapPreview from "../components/MapPreview";
+import {useSelector} from "react-redux";
+import Colors from "../constants/Colors";
 
 const PlaceDetailScreen = props => {
+    const placeId = props.navigation.getParam('placeId');
+
+    const selectedPlace = useSelector(state => state.places.places.find(place => place.id === placeId));
+
     return (
-        <View>
-            <Text>Place Detail Screen</Text>
-        </View>
+        <ScrollView contentContainerStyle={{alignItems: 'center'}}>
+            <Image style={styles.image} source={{ uri: selectedPlace.imageUri }}/>
+            <View style={styles.locationContainer}>
+                <View style={styles.addressContainer}>
+                    <Text style={styles.address}>{selectedPlace.address}</Text>
+                </View>
+                <MapPreview style={styles.mapPreview} location={{lat: selectedPlace.lat, lng: selectedPlace.lng}}/>
+            </View>
+        </ScrollView>
     )
 };
 
@@ -14,5 +28,42 @@ PlaceDetailScreen.navigationOptions = navData => {
         headerTitle: navData.navigation.getParam('placeTitle')
     }
 };
+
+const styles = StyleSheet.create({
+    image: {
+        height: '35%',
+        minHeight: 300,
+        width: '100%',
+        backgroundColor: '#ccc'
+    },
+    locationContainer: {
+        marginVertical: 20,
+        width: '90%',
+        maxWidth: 350,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        elevation: 5,
+        backgroundColor: 'white',
+        borderRadius: 10
+    },
+    addressContainer: {
+        padding: 20
+    },
+    address: {
+        color: Colors.primary,
+        textAlign: 'center'
+    },
+    mapPreview: {
+        width: '100%',
+        maxWidth: 350,
+        height: 300,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10
+    }
+});
 
 export default PlaceDetailScreen
