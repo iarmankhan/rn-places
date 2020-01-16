21aadc5cc7539261e2bacaf871936a0775b5ba12
iarmankhan <armankhan9244@gmail.com>
2020-01-16 12:15:05 +0530

    Setup SQLite db & created a new table

--- App.js
+++ App.js
@@ -4,7 +4,15 @@ import {Provider} from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 
 import PlacesNavigator from "./navigation/PlacesNavigator";
-import placesReducer from './store/reducers/places'
+import placesReducer from './store/reducers/places';
+
+import {init} from "./helpers/db";
+
+init().then(() => {
+    console.log("Initialized Database")
+}).catch(err => {
+    console.log('Initializing db failed', err)
+});
 
 const rootReducer = combineReducers({
   places: placesReducer
